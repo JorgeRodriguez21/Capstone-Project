0e4d46acc76a9696bb7bb784485f3ca3e409f691
Jorge Rodriguez <jlrodriguez@latamautos.com>
2016-06-22 17:52:44 -0500

    Commit inicial

--- Tagadata/app/src/main/java/com/app/bedomax/tagadata/activities/MainActivity.java
+++ Tagadata/app/src/main/java/com/app/bedomax/tagadata/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.app.bedomax.tagadata;
+package com.app.bedomax.tagadata.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.app.bedomax.tagadata.R;
 
 public class MainActivity extends AppCompatActivity {
 
