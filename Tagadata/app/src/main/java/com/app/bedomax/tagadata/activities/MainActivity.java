fcbd03e731444b14aa43c6c5aff4295269ded730
Jorge Rodriguez <jlrodriguez@latamautos.com>
2016-07-11 20:58:49 -0500

    se pinta el recycler view y la imagen el en banner principal

--- Tagadata/app/src/main/java/com/app/bedomax/tagadata/activities/MainActivity.java
+++ Tagadata/app/src/main/java/com/app/bedomax/tagadata/activities/MainActivity.java
@@ -1,31 +1,54 @@
 package com.app.bedomax.tagadata.activities;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.widget.LinearLayout;
 
 import com.app.bedomax.tagadata.R;
 import com.app.bedomax.tagadata.adapters.NewsRecyclerAdapter;
 import com.app.bedomax.tagadata.handlers.HandlerCallback;
 import com.app.bedomax.tagadata.handlers.NewsHandler;
 import com.app.bedomax.tagadata.models.New;
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.view.SimpleDraweeView;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    @InjectView(R.id.mainNewImage)
+    protected SimpleDraweeView mainNewImage;
+    @InjectView(R.id.newsRV)
+    protected RecyclerView newsRV;
+    @InjectView(R.id.profileToolbar)
+    protected Toolbar profileToobar;
     private NewsHandler handler;
     private NewsRecyclerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Fresco.initialize(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
         handler = new NewsHandler(this);
         adapter = new NewsRecyclerAdapter(this,handler.getNews());
+        GridLayoutManager manager = new GridLayoutManager(getApplicationContext(), 2);
+        newsRV.setLayoutManager(manager);
+        newsRV.setAdapter(adapter);
         handler.searchNews(new HandlerCallback.listener() {
             @Override
             public void onResponse(int status) {
-                for (New n : handler.getNews()){
-                    System.out.println(n.getName());
-                }
+                adapter.notifyDataSetChanged();
+                mainNewImage.setImageURI(Uri.parse(handler.getNews().get(0).getImage()));
+                profileToobar.setTitle(handler.getNews().get(0).getName());
             }
         });
 
--- Tagadata/app/src/main/java/com/app/bedomax/tagadata/adapters/NewsRecyclerAdapter.java
+++ Tagadata/app/src/main/java/com/app/bedomax/tagadata/adapters/NewsRecyclerAdapter.java
@@ -4,15 +4,22 @@ package com.app.bedomax.tagadata.adapters;
  * Created by Jorge on 22/06/16.
  */
 import android.content.Context;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-
+import android.widget.TextView;
 
 import com.app.bedomax.tagadata.R;
 import com.app.bedomax.tagadata.models.New;
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.interfaces.DraweeController;
+import com.facebook.drawee.view.SimpleDraweeView;
+import com.facebook.imagepipeline.common.ResizeOptions;
+import com.facebook.imagepipeline.request.ImageRequest;
+import com.facebook.imagepipeline.request.ImageRequestBuilder;
 
 import java.util.List;
 
@@ -22,6 +29,7 @@ public class NewsRecyclerAdapter extends RecyclerView.Adapter implements View.On
     private List<New> newsList;
     private View.OnClickListener listener;
     private OnLoadMoreListener onLoadMoreListener;
+    private static int width;
 
 
     public NewsRecyclerAdapter(Context context, List<New> news) {
@@ -35,6 +43,10 @@ public class NewsRecyclerAdapter extends RecyclerView.Adapter implements View.On
     }
 
 
+    private void getDimensions() {
+        DisplayMetrics displaymetrics = context.getResources().getDisplayMetrics();
+        width = displaymetrics.widthPixels / 3;
+    }
     public interface OnLoadMoreListener {
         void onLoadMore();
     }
@@ -45,10 +57,13 @@ public class NewsRecyclerAdapter extends RecyclerView.Adapter implements View.On
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
+        public TextView newsTitle;
+        public SimpleDraweeView newsPreviewImage;
 
         public ViewHolder(View view) {
             super(view);
-
+            newsTitle = (TextView) view.findViewById(R.id.newTitle);
+            newsPreviewImage = (SimpleDraweeView) view.findViewById(R.id.previewImage);
         }
     }
 
@@ -56,7 +71,7 @@ public class NewsRecyclerAdapter extends RecyclerView.Adapter implements View.On
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View v = null;
         RecyclerView.ViewHolder vh;
-        //v = LayoutInflater.from(context).inflate(R.layout.movie_list_content, parent, false);
+        v = LayoutInflater.from(context).inflate(R.layout.news_item, parent, false);
         vh = new ViewHolder(v);
         return vh;
     }
@@ -64,6 +79,19 @@ public class NewsRecyclerAdapter extends RecyclerView.Adapter implements View.On
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
         if (holder instanceof ViewHolder) {
+
+            ((ViewHolder) holder).newsTitle.setText(newsList.get(position).getName());
+            ImageRequest request;
+            getDimensions();
+            request = ImageRequestBuilder.newBuilderWithSource(Uri.parse(newsList.get(position).getImage()))
+                    .setResizeOptions(new ResizeOptions(width, width))
+                    .setAutoRotateEnabled(true)
+                    .build();
+            DraweeController controller = Fresco.newDraweeControllerBuilder()
+                    .setImageRequest((request))
+                    .setOldController(((ViewHolder) holder).newsPreviewImage.getController())
+                    .build();
+            ((ViewHolder) holder).newsPreviewImage.setController(controller);
            /* Picasso.with(context)
                     .load(context.getString(R.string.images_url_500)+moviesList.get(position).getPosterPath())
                     .into(((ViewHolder) holder).cover);
@@ -73,7 +101,7 @@ public class NewsRecyclerAdapter extends RecyclerView.Adapter implements View.On
     @Override
     public int getItemCount() {
         //return moviesList.size();
-        return 0;
+        return newsList.size();
     }
 
     public void setOnLoadMoreListener(OnLoadMoreListener onLoadMoreListener) {
